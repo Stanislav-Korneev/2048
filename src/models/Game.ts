780074e00dcc38e2217ccc43f20c07a361637411
Stanislav-Korneev <korstasvik@gmail.com>
2023-04-18 13:17:03 +0400

    united classes into one

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,18 +1,29 @@
-interface IGameData {
+type directionType = 'top' | 'down' | 'left' | 'right'
+export interface IGameData {
+    size: number
     score: number
+    grid: []
 }
 
 interface IGame extends IGameData {
     init: () => void
+    merge: (direction: directionType) => void
 }
 
 export default class Game implements IGame {
+    private readonly _size: number
     private _score: number
+    private _grid: []
 
-    constructor(payload: IGameData) {
-        this._score = payload.score;
+    constructor(settings: IGameData) {
+        this._score = settings.score;
+        this._grid = settings.grid;
+        this._size = settings.size;
     }
 
+    get size(): number {
+        return this._size;
+    }
     get score(): number {
         return this._score;
     }
@@ -20,8 +31,19 @@ export default class Game implements IGame {
         if (value < 0) return;
         this._score = value;
     }
+    get grid(): [] {
+        return this._grid;
+    }
+    set grid(value: []) {
+        this._grid = value;
+    }
 
     init(): void {
+        return;
+    }
+
+    merge(direction: directionType): void {
+        console.log(direction);
         return;
     }
 }
