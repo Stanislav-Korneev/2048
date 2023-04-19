ca5b22d39021dc708f5563210ff144c533f52a5e
Stanislav-Korneev <korstasvik@gmail.com>
2023-04-19 12:50:09 +0400

    added initializer for the game and basic funcs

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './style.css'
+import { IGameData } from "./models/Game";
+import Game from "./models/Game";
+
+const settings: IGameData = {
+    size: 5,
+    score: 0,
+    currentGrid: []
+}
+
+const game = new Game(settings);
+
+game.init();
--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,23 +1,28 @@
+import createDomElement from "../helpers/createDomElement";
+
 type directionType = 'top' | 'down' | 'left' | 'right'
+type gridItemType = number | null
 export interface IGameData {
     size: number
     score: number
-    grid: []
+    currentGrid: gridItemType[]
 }
 
 interface IGame extends IGameData {
     init: () => void
-    merge: (direction: directionType) => void
+    addNewItem: () => gridItemType[]
+    checkIsGameOver: () => boolean
+    merge: (direction: directionType) => gridItemType[]
 }
 
 export default class Game implements IGame {
     private readonly _size: number
     private _score: number
-    private _grid: []
+    private _currentGrid: gridItemType[]
 
     constructor(settings: IGameData) {
         this._score = settings.score;
-        this._grid = settings.grid;
+        this._currentGrid = settings.currentGrid;
         this._size = settings.size;
     }
 
@@ -31,19 +36,57 @@ export default class Game implements IGame {
         if (value < 0) return;
         this._score = value;
     }
-    get grid(): [] {
-        return this._grid;
+    get currentGrid(): gridItemType[] {
+        return this._currentGrid;
     }
-    set grid(value: []) {
-        this._grid = value;
+    set currentGrid(value: gridItemType[]) {
+        this._currentGrid = value;
+    }
+
+    get powSize(): number {
+        return Math.pow(this._size, 2);
     }
 
     init(): void {
-        return;
+        const container = document.getElementById('container')!;
+
+        const grid = createDomElement({
+            classList: ['grid'],
+            parent: container,
+        })
+
+        this._currentGrid = new Array(this.powSize).fill(null);
+        this.addNewItem();
+        console.log('this._currentGrid', this._currentGrid)
+
+        this.currentGrid.forEach((item) => createDomElement({
+            classList: ['grid-item'],
+            textContent: item ? item.toString() : '',
+            parent: grid,
+        }))
+    }
+
+    addNewItem(): gridItemType[] {
+        const newItem: 2|4 = (Math.random() * (11 - 1) + 1) < 9 ? 2 : 4;
+        const result = [...this._currentGrid];
+
+        const addToRandomSlot = (): gridItemType[] => {
+            const newIndex = Math.floor(Math.random() * (this.powSize + 1));
+            if (result[newIndex] !== null) return addToRandomSlot();
+            result[newIndex] = newItem;
+            return result;
+        }
+        this._currentGrid = result;
+        return addToRandomSlot();
+    }
+
+    checkIsGameOver(): boolean {
+        return !this._currentGrid.find(item => item === null);
     }
 
     merge(direction: directionType): void {
         console.log(direction);
         return;
     }
+
 }
